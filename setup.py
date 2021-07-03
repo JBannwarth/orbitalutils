7b6140182d3dca69d79eac1020036159e387876c
Jérémie Bannwarth <jban039@aucklanduni.ac.nz>
2021-07-03 13:17:20 +1200

    Fix incorrect package name

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='jban039@aucklanduni.ac.nz',
     packages=['orbitutils', 'orbitutils.test'],
     # scripts=['bin/script1','bin/script2'],
-    url='https://github.com/JBannwarth/OrbitalMechanics',
+    url='https://github.com/JBannwarth/orbitutils',
     license='LICENSE',
     description='Utilities to solve orbital mechanics problems, based on ' + \
         'Orbital Mechanics for Engineering Students (4th Edition) by Howard ' + \
